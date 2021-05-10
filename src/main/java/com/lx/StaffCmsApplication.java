8c0174ecfa8195a3dba54278ca382a19889a268c
zjh <1171111151@qq.com>
2021-05-10 17:26:16 +0800

    '项目初始化'

---
+++ src/main/java/com/lx/StaffCmsApplication.java
@@ -0,0 +1,13 @@
+package com.lx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaffCmsApplication.class, args);
+    }
+
+}
